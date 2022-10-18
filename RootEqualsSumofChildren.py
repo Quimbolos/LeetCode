869743f45a81f36f2812434f9f305076d345b6e8
Joaquim Bolós Fernández <93405477+Quimbolos@users.noreply.github.com>
2022-10-18 14:44:27 +0100

    Create RootEqualsSumofChildren.py

---
+++ RootEqualsSumofChildren.py
@@ -0,0 +1,20 @@
+'''
+You are given the root of a binary tree that consists of exactly 3 nodes: the root, its left child, and its right child.
+
+Return true if the value of the root is equal to the sum of the values of its two children, or false otherwise.
+
+https://leetcode.com/problems/root-equals-sum-of-children/
+'''
+
+class Solution(object):
+    def checkTree(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        if root.val == (root.left.val +root.right.val):
+            sol = True
+        else:
+            sol = False
+        
+        return(sol)
