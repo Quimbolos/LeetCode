6d200ccaf8b10c38e4b38bf7951cc03d912292f8
Joaquim Bolós Fernández <93405477+Quimbolos@users.noreply.github.com>
2022-10-18 14:43:27 +0100

    Create AddTwoIntigers.py

---
+++ AddTwoIntigers.py
@@ -0,0 +1,16 @@
+'''
+Given two integers num1 and num2, return the sum of the two integers.
+
+https://leetcode.com/problems/add-two-integers/
+'''
+
+class Solution(object):
+    def sum(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        number = num1 + num2
+        
+        return(number)
