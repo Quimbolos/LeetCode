738f30a4728dcf2d93cf75232629fa6801b194ce
Joaquim Bolós Fernández <93405477+Quimbolos@users.noreply.github.com>
2022-10-18 13:02:50 +0100

    Update and rename LongestCommonPrefix to LongestCommonPrefix.py

--- LongestCommonPrefix.py
+++ LongestCommonPrefix.py
@@ -2,6 +2,8 @@
 Write a function to find the longest common prefix string amongst an array of strings.
 
 If there is no common prefix, return an empty string "".
+
+https://leetcode.com/problems/longest-common-prefix/
 '''
 
 class Solution(object):
